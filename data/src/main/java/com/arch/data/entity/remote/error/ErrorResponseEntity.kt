9466dee50da705e34efa195650eaf9d0c70ff9ac
Deep Naik <naikdp7@gmail.com>
2021-12-29 17:13:01 +0530

    ->removed unwanted error class in error response entity

--- data/src/main/java/com/arch/data/entity/remote/error/ErrorResponseEntity.kt
+++ data/src/main/java/com/arch/data/entity/remote/error/ErrorResponseEntity.kt
@@ -6,12 +6,4 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class ErrorResponseEntity(
     @Json(name = "error") var error: String? = null
-) {
-    @JsonClass(generateAdapter = true)
-    data class Error(
-        @Json(name = "code") var code: String?,
-        @Json(name = "message") var message: String?,
-        @Json(name = "msg") var msg: String?,
-        @Json(name = "requestId") var requestId: String?
-    )
-}
+)
