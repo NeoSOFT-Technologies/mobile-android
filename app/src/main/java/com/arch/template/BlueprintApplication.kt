205ee94c61641fedc29e798fcffcd2e5a12db117
Deep Naik <naikdp7@gmail.com>
2022-01-11 19:26:25 +0530

    ->crashlytics integrated

--- app/src/main/java/com/arch/template/BlueprintApplication.kt
+++ app/src/main/java/com/arch/template/BlueprintApplication.kt
@@ -2,6 +2,7 @@ package com.arch.template
 
 import android.app.Application
 import com.arch.template.utils.AppExceptionMapper
+import com.arch.template.utils.CrashlyticsLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,5 +10,6 @@ class BlueprintApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         AppExceptionMapper.init(this)
+        CrashlyticsLogger.init(this)
     }
 }
